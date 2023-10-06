eee681ee48ed9c6c1be1b151a9c2d3ae792cfcd2
DiepTCNN <DiepTCNN@10.12.49.128>
2023-10-06 14:27:42 +0700

    DoctorManage

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+import controller.DoctorManagement;
+
+
+
+/**
+ *
+ * @author DiepTCNN
+ */
+public class Main {
+    public static void main(String[] args) {
+        DoctorManagement doctorManagement = new DoctorManagement();
+        doctorManagement.run();
+    }
+}
